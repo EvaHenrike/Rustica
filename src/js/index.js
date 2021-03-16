fe08dc794c62d95e13e857e8545970eef131fdd4
Eva Flandermeier <eva.flandermeier@gmail.com>
2021-03-16 10:52:14 +0100

    Shop HTML

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,9 @@ myFunction();
 const result = multiply(2, 5);
 
 console.log(result);
+
+const body = document.body;
+const navi = document.getElementsByClassName(navigation_divI)
+const navifont = document.getElementsByClassName(navigation_linkI)
+navigation_divI
+navigation_divI.style.backgroundColor = 'black'
